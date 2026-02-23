8b78ac1b12df1352ba5acabdebb40f2ef6bf508e
nasronasro <ahfir49481@gmail.com>
2026-02-23 11:20:05 +0000

    initial commit

---
+++ AlgorithmicProblemsStorage/Presentation/Forms/MainForm.cs
@@ -0,0 +1,10 @@
+namespace AlgorithmicProblemsStorage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
